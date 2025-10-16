4ec3550a264b2a161b55e74e511d858d3ed1a82d
deveploper99 <rtxdeveploper99@gmail.com>
2025-10-16 21:56:52 +0600

    addition Subtraction multiplication and divition added for kotlin

--- app/src/main/java/com/example/smcalculator/MainActivity.kt
+++ app/src/main/java/com/example/smcalculator/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.smcalculator
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.toString
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +15,58 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val edit_text_1 = findViewById<EditText>(R.id.firstNumber)
+        val edit_text_2 = findViewById<EditText>(R.id.secondNumber)
+        val addition = findViewById<TextView>(R.id.add)
+        val subtraction = findViewById<TextView>(R.id.sub)
+        val multiplication = findViewById<TextView>(R.id.multi)
+        val divition = findViewById<TextView>(R.id.div)
+        val displa = findViewById<TextView>(R.id.display)
+
+
+
+
+        addition.setOnClickListener {
+
+
+            val number1 = edit_text_1.text.toString().toInt()
+            val number2 = edit_text_2.text.toString().toInt()
+            val result = number1 + number2
+            displa.text = "$result"
+
+
+        }
+
+
+        subtraction.setOnClickListener {
+            val number1 = edit_text_1.text.toString().toInt()
+            val number2 = edit_text_2.text.toString().toInt()
+
+            val result = number1-number2
+            displa.text = "$result"
+        }
+
+
+        multiplication.setOnClickListener {
+            val number1 = edit_text_1.text.toString().toInt()
+            val number2 = edit_text_2.text.toString().toInt()
+
+            val result = number1*number2
+            displa.text = "$result"
+        }
+
+        divition.setOnClickListener {
+            val number1 = edit_text_1.text.toString().toInt()
+            val number2 = edit_text_2.text.toString().toInt()
+            val result = number1+number2
+            displa.text = "$result"
+        }
+
+
+
+
+
     }
+
+
 }
